ec58463c89983f2cff32244b75ff7cb42ec8961c
mttias <simon.koser9+github@gmail.com>
2024-10-25 10:37:43 +0100

    add comments

--- main2.py
+++ main2.py
@@ -14,6 +14,65 @@ class GPTConfig:
     n_embd: int = 768
 
 
+class CausalSelfAttention(nn.Modue):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+
+        # weights. once we multiply our input (n_embd * 1) with (3*n_embd x n_embd),
+        # we get a (3*n_embd * 1) matrix.
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # Weight matrix is (3*n_embd, n_embd)
+
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+        # not really a bias, more of a mask, but following the oAI/HF naming
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
+
+    def forward(self, x):
+        """
+        x: (batch_size, seq_length, n_embd)
+        """
+        B, T, C = x.size()  # (batch_size, seq_length, n_embd)
+
+        # the c_attn (lin. proj) operates on the last dimension of x,
+        # i.e. (n_embd * 1) x (3*n_embd x n_embd) -> (3*n_embd * 1)
+        # splits into q, k, v (n_embd, n_embd) each
+        # dim=2 refers to the third dimension
+        # this splits the (batch_size, seq_length, 3*n_embd) matrix into
+        # three different 3d tensors (batch_size, seq_length, n_embd)
+        # n_embd_q is query, n_embd_k is key, n_embd_v is value
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+
+        # points to same range in memory (contiguous), but reshapes
+        # self.n_head = 12, C = 768, so we're splitting into 12 heads of 64 dim
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        attn = attn.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        attn = F.softmax(attn, dim=-1)
+
+        y = attn @ v  # (B, nh, T, hs)
+
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+
+        y = self.c_proj(y)
+
+        return y
+
+
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
